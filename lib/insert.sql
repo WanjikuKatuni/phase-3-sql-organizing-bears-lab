6e90ec832ef00b8180ec07d98cc9c99c6d596773
wanjiku <annritawanjiku77@gmail.com>
2022-11-04 13:48:46 +0300

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr.Chocolate", 3, "F", "brown","sanguine", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 8, "F", "brown","melancholic", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 2, "M", "black","sanguine", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 16, "F", "brown","phlegmatic", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 5, "M", "white","sanguine", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 7, "M", "black","choleric", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 22, "F", "brown and white","phlegmatic", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 16, "F", "grey","choleric", 1);
